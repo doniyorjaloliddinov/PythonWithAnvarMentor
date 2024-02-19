2fe9e3afca07b8be3da616129ebdf4e43f3124f5
doni <you@example.com>
2024-02-19 15:12:35 +0500

    add:students module

---
+++ students.py
@@ -0,0 +1,29 @@
+class Student:
+    __cuantity_of_students = 0
+ 
+    def __init__(self, name, b_year,gender, grade=1):
+        self.name = name
+        self.b_year = b_year
+        self.grade = grade
+        self.subjects = []
+        self.__gender = gender
+        Student.__cuantity_of_students +=1
+
+    def get_gender(self):
+        return self.__gender
+    
+    def register_to_subject(self, subject):
+        self.subjects.append(subject)
+        for sub in self.subjects:  
+            return sub 
+
+    def remove_subject(self,removing_subject):
+        if removing_subject in self.subjects:
+            self.subjects.remove(removing_subject)
+            result = print(f"{removing_subject} is removed!")
+        else:
+            result = print(f"{removing_subject} is not Found!")
+
+    @classmethod
+    def get_cuantity_of_students(cls):
+        return cls.__cuantity_of_students
